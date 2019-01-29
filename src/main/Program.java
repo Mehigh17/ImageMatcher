94b9ec3399f7e0761a98e0d9df69a6d28f5c360a
Mihai <kazepworld@gmail.com>
2019-01-29 15:18:47 +0100

    Increase startup resolution

--- src/main/Program.java
+++ src/main/Program.java
@@ -12,7 +12,7 @@ public class Program extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/MainView.fxml"));
         primaryStage.setTitle("Image Matcher");
-        primaryStage.setScene(new Scene(root, 600, 455));
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
